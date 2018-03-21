b8c6c14d812476642cd81b3c8a2c26fd0c95bbae
Paulo Raposo <pauloj.raposo@outlook.com>
2018-03-20 23:35:08 -0400

    Reproduced NASA's ApplicationTemplate. App is two classes, with Main providing the program entry point and MainGUI handling the GUI and WorldWind elements.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # DGGSPointViewer
+
+An app using NASA's WorldWind virtual globe to visualize point datasets on the globe aggregated to various resolutions on a discrete global grid system (DGGS), namely Dutton's QTM.
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+
+        MainGUI mGUI = new MainGUI();
+        mGUI.start("World Wind Application", MainGUI.AppFrame.class);
+
+    }
+
+
+}
